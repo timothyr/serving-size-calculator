173c2931630ec31e1891d86c775d981adef96b96
Adriel <adrielr@sfu.ca>
2017-02-02 03:18:48 -0800

    testing cloning

--- app/src/main/java/sfu/timr/servingsizecalculator/MainActivity.java
+++ app/src/main/java/sfu/timr/servingsizecalculator/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+// 👑
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
