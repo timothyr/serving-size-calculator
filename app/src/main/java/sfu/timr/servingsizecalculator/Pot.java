cba9590e64f43f5c48f51be263ec5c75a9521cfc
Tim Robertson <timr@sfu.ca>
2017-02-02 03:06:52 -0800

    Implement Pot methods

--- app/src/main/java/sfu/timr/servingsizecalculator/Pot.java
+++ app/src/main/java/sfu/timr/servingsizecalculator/Pot.java
@@ -6,31 +6,38 @@ package sfu.timr.servingsizecalculator;
 
 public class Pot {
 
+    private String name;
+    private int weightInG;
     // Set member data based on parameters.
     public Pot(String name, int weightInG) {
-        //TODO
+        this.name = name;
+        this.weightInG = weightInG;
     }
 
     // Return the weight
     public int getWeightInG() {
-        //TODO
-        return 0;
+        return weightInG;
     }
 
     // Set the weight. Throws IllegalArgumentException if weight is less than 0.
-    public void setWeightInG(int weightInG) {
-        //TODO
+    public void setWeightInG(int weightInG) throws IllegalArgumentException {
+        if(weightInG < 0) {
+            throw new IllegalArgumentException("Weight must be greater than or equal to 0");
+        }
+        this.weightInG = weightInG;
     }
 
     // Return the name.
     public String getName() {
-        //TODO
-        return "";
+        return name;
     }
 
     // Set the name. Throws IllegalArgumentException if name is an empty string (length 0),
     // or if name is a null-reference.
-    public void setName(String name) {
-        //TODO
+    public void setName(String name) throws IllegalArgumentException {
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Invalid Pot name");
+        }
+        this.name = name;
     }
 }
