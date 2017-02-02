18e3cba101233af8811efb2c30603f35a4cea0db
Tim Robertson <timr@sfu.ca>
2017-02-02 02:55:10 -0800

    Add Pot Add PotCollection

---
+++ app/src/main/java/sfu/timr/servingsizecalculator/Pot.java
@@ -0,0 +1,36 @@
+package sfu.timr.servingsizecalculator;
+
+/**
+ * Store information about a single pot
+ */
+
+public class Pot {
+
+    // Set member data based on parameters.
+    public Pot(String name, int weightInG) {
+        //TODO
+    }
+
+    // Return the weight
+    public int getWeightInG() {
+        //TODO
+        return 0;
+    }
+
+    // Set the weight. Throws IllegalArgumentException if weight is less than 0.
+    public void setWeightInG(int weightInG) {
+        //TODO
+    }
+
+    // Return the name.
+    public String getName() {
+        //TODO
+        return "";
+    }
+
+    // Set the name. Throws IllegalArgumentException if name is an empty string (length 0),
+    // or if name is a null-reference.
+    public void setName(String name) {
+        //TODO
+    }
+}
