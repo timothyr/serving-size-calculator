dc73a1536a627ca13944a2c13a9771f001d948d7
Tim Robertson <timr@sfu.ca>
2017-02-03 00:47:41 -0800

    Add hint to textboxes

--- app/src/main/java/sfu/timr/servingsizecalculator/EnterPotActivity.java
+++ app/src/main/java/sfu/timr/servingsizecalculator/EnterPotActivity.java
@@ -9,5 +9,7 @@ public class EnterPotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_pot);
+
+
     }
 }
