7d792d93d7b8579fb2a9fbcf54256cf783d6e5f1
Adriel <adrielr@sfu.ca>
2017-02-02 23:15:29 -0800

    Adding new files

---
+++ app/src/main/java/sfu/timr/servingsizecalculator/CalculatePot.java
@@ -0,0 +1,14 @@
+package sfu.timr.servingsizecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatePot extends AppCompatActivity {
+
+    // For use with calculating the serving size in a pot.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_pot);
+    }
+}
