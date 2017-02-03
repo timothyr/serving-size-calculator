f8cd968650abdbc96262a29bc0cb581a2bcca9cb
Tim Robertson <timr@sfu.ca>
2017-02-02 23:09:51 -0800

    Add addPot

--- app/src/main/java/sfu/timr/servingsizecalculator/MainActivity.java
+++ app/src/main/java/sfu/timr/servingsizecalculator/MainActivity.java
@@ -22,11 +22,17 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                addPot();
             }
         });
     }
+
+    private void addPot() {
+
+    }
+
+    // Default boilerplate that might come handy later
+    // -----------------------------
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
