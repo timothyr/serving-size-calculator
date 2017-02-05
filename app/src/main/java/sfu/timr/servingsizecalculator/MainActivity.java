03ff0565ac40fcd714deb1eec6a36b064119868c
Tim Robertson <timr@sfu.ca>
2017-02-05 14:15:46 -0800

    Add Edit and Delete to ActionBar

--- app/src/main/java/sfu/timr/servingsizecalculator/MainActivity.java
+++ app/src/main/java/sfu/timr/servingsizecalculator/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -25,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //toolbar.showContextMenu();
+        toolbar.showOverflowMenu();
+
+        //ActionBar actionBar = getSupportActionBar();
+        //actionBar.show();
 
         setupFloatingAddPotButton();
 
@@ -97,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
     // Default boilerplate that might come handy later
     // -----------------------------
 
-    /*
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -112,12 +121,14 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        /*
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+        */
 
         return super.onOptionsItemSelected(item);
     }
-    */
+
 }
