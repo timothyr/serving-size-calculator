57794560cd84d47730d662358924730d18fec6b1
Adriel <adrielr@sfu.ca>
2017-02-05 17:10:21 -0800

    Created skeleton of Calculate Pot Activity

--- app/src/main/java/sfu/timr/servingsizecalculator/CalculateServingActivity.java
+++ app/src/main/java/sfu/timr/servingsizecalculator/CalculateServingActivity.java
@@ -2,6 +2,8 @@ package sfu.timr.servingsizecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalculateServingActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class CalculateServingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate_pot);
+
+        setupBackButton();
+    }
+
+    private void setupBackButton() {
+        Button backButton = (Button) findViewById(R.id.calculate_pot_back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
+
+
